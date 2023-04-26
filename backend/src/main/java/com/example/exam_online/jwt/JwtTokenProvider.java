13ede52680afa2944b3541350fe43eb4ee4b854a
ndkhang_java <ndkhang.itnlu@gmail.com>
2023-04-26 20:06:26 +0700

    Create jwt token

---
+++ backend/src/main/java/com/example/exam_online/jwt/JwtTokenProvider.java
@@ -0,0 +1,57 @@
+package com.example.exam_online.jwt;
+
+import com.example.exam_online.entity.CustomUserDetails;
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtTokenProvider {
+    // JWT_SECRET is secret
+    private final String JWT_SECRET = "pdsteam";
+
+    //Available time of jwt
+    private final long JWT_EXPIRATION = 604800000L;
+
+    // Generate jwt from user infor
+    public String generateToken(CustomUserDetails userDetails) {
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + JWT_EXPIRATION);
+        // Generate json web token from id of user.
+        return Jwts.builder()
+                .setSubject(Long.toString(userDetails.getUser().getIdUser()))
+                .setIssuedAt(now)
+                .setExpiration(expiryDate)
+                .signWith(SignatureAlgorithm.HS512, JWT_SECRET)
+                .compact();
+    }
+
+    // Get information of user from jwt
+    public Long getUserIdFromJWT(String token) {
+        Claims claims = Jwts.parser()
+                .setSigningKey(JWT_SECRET)
+                .parseClaimsJws(token)
+                .getBody();
+
+        return Long.parseLong(claims.getSubject());
+    }
+
+    public boolean validateToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(JWT_SECRET).parseClaimsJws(authToken);
+            return true;
+        } catch (MalformedJwtException ex) {
+            log.error("Invalid JWT token");
+        } catch (ExpiredJwtException ex) {
+            log.error("Expired JWT token");
+        } catch (UnsupportedJwtException ex) {
+            log.error("Unsupported JWT token");
+        } catch (IllegalArgumentException ex) {
+            log.error("JWT claims string is empty.");
+        }
+        return false;
+    }
+}
